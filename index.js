8b3323e5e9a0da72f4a93c0b86c04ab6d84c69ae
rose <144841421+rosenode@users.noreply.github.com>
2025-06-18 10:42:49 -0400

    1

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    .then(response => {
+        if(!response.ok){
+            throw new Error("Could not fetch resource");
+            return response.json();
+        }
+    }
+.then(data => console.log(data.id))
+.catch(error => console.error(error));
