bd122723284842b32b502aaba9d511aaa977c645
rose <144841421+rosenode@users.noreply.github.com>
2025-06-18 11:57:10 -0400

    Complete

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-    .then(response => {
+async function fetchData(){
+
+    try{
+
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
         if(!response.ok){
             throw new Error("Could not fetch resource");
-            return response.json();
         }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
     }
-.then(data => console.log(data.id))
-.catch(error => console.error(error))
+    catch(error){
+        console.error(error);
+    }
+}
